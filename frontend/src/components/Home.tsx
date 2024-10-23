55776b5b6db7b6749af4f6131556a2fa9e06dcef
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-10-23 12:34:55 -0600

    Adding client to homepage

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,15 @@
 import { Amplify } from 'aws-amplify';
 import { Heading, Text, View } from '@aws-amplify/ui-react';
 import outputs from "../../amplify_outputs.json";
+import type { Schema } from '../../amplify/data/resource';
+import { generateClient } from 'aws-amplify/api';
 
+const client = generateClient<Schema>();
 Amplify.configure(outputs);
 
-function Home() {  
 
+function Home() {  
+  
   return (
     <View padding="1rem">
         <Heading level={1}>Home</Heading>

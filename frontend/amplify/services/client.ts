133536d5943cd777f0b16f477e64cccaf55a48c4
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-10-23 19:55:15 -0600

    Trying manually configuring the amplify outputs

--- frontend/amplify/services/client.ts
+++ frontend/amplify/services/client.ts
@@ -1,12 +1,19 @@
 import { Amplify } from '@aws-amplify/core';
-import outputs from "../outputs.json";
-import { generateClient } from "@aws-amplify/api";
 import type { Schema } from "../data/resource";
+import { generateClient } from "@aws-amplify/api";
 
-Amplify.configure(outputs);
+const amplifyConfig = {
+  aws_project_region: process.env.AWS_REGION,
+  aws_appsync_graphqlEndpoint: process.env.API_ENDPOINT,
+  aws_appsync_apiKey: process.env.API_KEY,
+  aws_appsync_region: process.env.AWS_REGION,
+  aws_appsync_authenticationType: 'API_KEY'
+};
+
+Amplify.configure(amplifyConfig);
 
 const client = generateClient<Schema>({
-    authMode: 'apiKey'
-  });
+  authMode: 'apiKey'
+});
 
 export default client;

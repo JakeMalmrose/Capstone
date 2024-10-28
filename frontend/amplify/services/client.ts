515c684fe8097e04bc5868512d02c525dff6bc2e
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-10-28 12:09:13 -0600

    Switching over to 2 different outputs determined by environment variables

--- frontend/amplify/services/client.ts
+++ frontend/amplify/services/client.ts
@@ -1,16 +1,10 @@
 import { Amplify } from '@aws-amplify/core';
 import { generateClient } from "@aws-amplify/api";
 import type { Schema } from "../data/resource";
-import outputs from "./outputs.json";
+import cloudOutputs from "./cloudOutputs.json";
+import localOutputs from "./localOutputs.json";
 
-
-const amplifyConfig = {
-  aws_project_region: process.env.REGION,
-  aws_appsync_graphqlEndpoint: process.env.API_ENDPOINT,
-  aws_appsync_apiKey: process.env.API_KEY,
-  aws_appsync_region: process.env.REGION,
-  aws_appsync_authenticationType: 'API_KEY'
-};
+const outputs = process.env.NODE_ENV === 'production' ? cloudOutputs : localOutputs;
 
 Amplify.configure(outputs);
 

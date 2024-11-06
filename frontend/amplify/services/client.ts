2e0a88b8c544fed36ec9720407ca0ce2ea711cee
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-11-05 18:32:03 -0700

    Update for running on cloud url

--- frontend/amplify/services/client.ts
+++ frontend/amplify/services/client.ts
@@ -1,16 +1,11 @@
 import { Amplify } from '@aws-amplify/core';
 import { generateClient } from "@aws-amplify/api";
 import type { Schema } from "../data/resource";
+//import localOutputs from "../../amplify_outputs.json";
+// Amplify.configure(localOutputs);
+
 import cloudOutputs from "./cloudOutputs2.json";
-import localOutputs from "../../amplify_outputs.json";
-
-const isLocal = false;
-
-if (isLocal) {
-  Amplify.configure(localOutputs);
-} else {
-  Amplify.configure(cloudOutputs);
-}
+Amplify.configure(cloudOutputs);
 
 const client = generateClient<Schema>({
     authMode: 'apiKey'

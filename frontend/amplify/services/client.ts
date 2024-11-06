5e205c9c5d764e3c62c212c6976b28a7136dce6f
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-11-06 09:04:31 -0700

    Let's pretend you cant see this commit

--- frontend/amplify/services/client.ts
+++ frontend/amplify/services/client.ts
@@ -1,11 +1,11 @@
 import { Amplify } from '@aws-amplify/core';
 import { generateClient } from "@aws-amplify/api";
 import type { Schema } from "../data/resource";
-import localOutputs from "../../amplify_outputs.json";
-Amplify.configure(localOutputs);
+// import localOutputs from "../../amplify_outputs.json";
+// Amplify.configure(localOutputs);
 
-// import cloudOutputs from "./cloudOutputs2.json";
-// Amplify.configure(cloudOutputs);
+import cloudOutputs from "./cloudOutputs2.json";
+Amplify.configure(cloudOutputs);
 
 const client = generateClient<Schema>({
     authMode: 'apiKey'

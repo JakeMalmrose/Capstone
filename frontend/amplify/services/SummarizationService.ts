28e9e0083ab9d0911e449788dac25aa11436cca9
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-11-11 19:19:04 -0700

    Removing extra import

--- frontend/amplify/services/SummarizationService.ts
+++ frontend/amplify/services/SummarizationService.ts
@@ -1,4 +1,3 @@
-import { generateClient } from 'aws-amplify/data';
 import type { Schema } from '../../amplify/data/resource';
 import { LLMClient, LLMConfig, LLMMessage } from './llmClients/LLMClient';
 import client from './client';
